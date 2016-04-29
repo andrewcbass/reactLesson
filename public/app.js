075c6601e7489b3c62e94b9fdcb54e41f490ddc7
andrewcbass <andrew.c.bass@gmail.com>
2016-04-29 16:51:05 -0700

    end of day1

--- public/app.js
+++ public/app.js
@@ -3,8 +3,39 @@ import ReactDOM from "react-dom";
 
 import List from "./components/List";
 import Book from "./components/Book";
+import Form from "./components/Form";
 
-window.I = ReactDOM.render(
-  <List component={Book}/>,
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [
+        { id: 1, name: "Learn React", price: 2999 },
+        { id: 2, name: "Learn Flux"},
+        { id: 3, name: "Learn GraphQL", price: 0 },
+        { id: 4, name: "Learn Relay", price: 4999 }
+      ],
+    };
+    this.addItem = this.addItem.bind(this);
+  }
+  addItem(newBook) {
+    const currentItems = this.state.items;
+    this.setState({
+      items: currentItems.concat(newBook)
+    });
+  }
+  render() {
+    return (
+      <div>
+        <List items={this.state.items} component={Book} />
+        <Form addItem={this.addItem}/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
   document.getElementById("react")
 );
--- public/components/Book.js
+++ public/components/Book.js
@@ -1,10 +1,23 @@
 import React from "react";
 
 class Book extends React.Component {
+  priceDisplay() {
+    const { price } = this.props;
+    if (price !== undefined) {
+      return (
+        <span>
+          &nbsp;-&nbsp;
+          {price === 0 ? "FREE" :  price/100}
+        </span>
+      );
+    }
+  }
   render() {
     return (
       <li>
-        {this.props.id}: {this.props.name} - {this.props.price}
+        {this.props.id} : {this.props.name}
+        
+        {this.priceDisplay()}
       </li>
     )
   }
